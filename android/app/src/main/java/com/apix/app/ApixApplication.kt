b8ceb80152038146abe663465f6b2da2d3759a78
a-pix <apix.plus@gmail.com>
2026-05-07 19:22:59 +0300

    Update ApixApplication.kt

--- android/app/src/main/java/com/apix/app/ApixApplication.kt
+++ android/app/src/main/java/com/apix/app/ApixApplication.kt
@@ -3,6 +3,7 @@ package com.apix.app
 import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import com.apix.app.security.KeysVault
 import coil.disk.DiskCache
 import coil.memory.MemoryCache
 import coil.request.CachePolicy
