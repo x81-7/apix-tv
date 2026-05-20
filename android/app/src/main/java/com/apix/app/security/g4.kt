627ac42f0b1a85887c9d0db4bffffb8ae7a90cf0
a-pix <apix.plus@gmail.com>
2026-05-20 13:03:44 +0300

    Update g4.kt

--- android/app/src/main/java/com/apix/app/security/g4.kt
+++ android/app/src/main/java/com/apix/app/security/g4.kt
@@ -1,7 +1,24 @@
-@JvmStatic fun ka(): String = a()
+package com.apix.app.security
+
+object g4 {
+
+    init {
+        System.loadLibrary("v")
+    }
+
+    private external fun a(): String
+    private external fun b(): String
+    private external fun c(): String
+    private external fun d(): String
+    private external fun e(): String
+    private external fun f(): String
+    private external fun g(): String
+
+    @JvmStatic fun ka(): String = a()
     @JvmStatic fun kb(): String = b()
     @JvmStatic fun kc(): String = c()
     @JvmStatic fun kd(): String = d()
     @JvmStatic fun ke(): String = e()
     @JvmStatic fun kf(): String = f()
     @JvmStatic fun kg(): String = g()
+}
