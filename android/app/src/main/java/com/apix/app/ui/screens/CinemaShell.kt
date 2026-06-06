3b3061dc35e4c16cdea43d7838dd754231fa73bb
a-pix <apix.plus@gmail.com>
2026-06-06 08:06:14 +0300

    Update CinemaShell.kt

--- android/app/src/main/java/com/apix/app/ui/screens/CinemaShell.kt
+++ android/app/src/main/java/com/apix/app/ui/screens/CinemaShell.kt
@@ -11,7 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
+
+
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
