a98cb5b1d0305f679541e573dd454b9d958d67ee
x81-7 <hadeelhasson2007@gmail.com>
2026-06-20 17:16:45 +0300

    Update PayloadCipher.swift

--- ios/APiXTV/Services/PayloadCipher.swift
+++ ios/APiXTV/Services/PayloadCipher.swift
@@ -12,7 +12,7 @@ enum PayloadCipher {
     }
 
     private static let fallbackKeyHex =
-        "0000000000000000000000000000000000000000000000000000000000000000"
+        "7a3f8b9d4e2c1a5f6b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a"
 
     private static let key: SymmetricKey = {
         let raw: String = {
