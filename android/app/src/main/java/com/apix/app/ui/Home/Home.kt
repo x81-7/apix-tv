cbb9e375fcd12ddbe8414db8eb988d353332f03a
x81-7 <hadeelhasson2007@gmail.com>
2026-08-20 18:42:27 +0300

    Update Home.kt

--- android/app/src/main/java/com/apix/app/ui/Home/Home.kt
+++ android/app/src/main/java/com/apix/app/ui/Home/Home.kt
@@ -27,8 +27,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.shadow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LiveTv
 import androidx.compose.material.icons.filled.Movie
