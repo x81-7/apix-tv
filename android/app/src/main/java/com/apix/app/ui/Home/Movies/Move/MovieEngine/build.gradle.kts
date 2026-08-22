6e6d5669b41f73e05ffa558c99059c17d059730c
x81-7 <hadeelhasson2007@gmail.com>
2026-08-22 19:25:54 +0300

    Update build.gradle.kts

--- android/app/src/main/java/com/apix/app/ui/Home/Movies/Move/MovieEngine/build.gradle.kts
+++ android/app/src/main/java/com/apix/app/ui/Home/Movies/Move/MovieEngine/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.library)
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     alias(libs.plugins.kotlin.serialization)
 }
