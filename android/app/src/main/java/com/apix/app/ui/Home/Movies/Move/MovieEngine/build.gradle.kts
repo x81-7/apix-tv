4e7dfb3a64fd04595a964f91507f52a57ada8246
x81-7 <hadeelhasson2007@gmail.com>
2026-08-22 20:04:50 +0300

    Update build.gradle.kts

--- android/app/src/main/java/com/apix/app/ui/Home/Movies/Move/MovieEngine/build.gradle.kts
+++ android/app/src/main/java/com/apix/app/ui/Home/Movies/Move/MovieEngine/build.gradle.kts
@@ -1,25 +1,15 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    id("com.android.library")
+    alias(libs.plugins.android.library)
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.serialization")
+    id("org.jetbrains.kotlin.plugin.compose")
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.lagradost.cloudstream3"
-
-    sourceSets {
-        getByName("main") {
-            java.srcDirs(
-                "../Player",
-                "../Plugs",
-                "../ui",
-                "../Core/Shared",
-                "../Core/CoreLibraryCompat/src/main/java"
-            )
-        }
-    }
-    
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         minSdk = 23
         consumerProguardFiles("consumer-rules.pro")
@@ -39,17 +29,26 @@ android {
         buildConfigField("int", "VERSION_CODE", "68")
     }
     buildFeatures { buildConfig = true; viewBinding = true; compose = true }
-    
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.8"
-    }
-    
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
         isCoreLibraryDesugaringEnabled = true
     }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
+}
+
+
+kotlin {
+    sourceSets.getByName("main").kotlin.srcDirs(
+        "../Player",
+        "../Plugs",
+        "../ui",
+        "../Core/Shared",
+        "../Core/CoreLibraryCompat"
+    )
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
@@ -74,13 +73,13 @@ dependencies {
     implementation(libs.colorpicker)
     implementation(libs.newpipeextractor)
     implementation(libs.juniversalchardet)
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.foundation:foundation:1.5.4")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation("androidx.compose.material:material-icons-core:1.5.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.activity:activity-compose:1.13.0")
+    implementation("androidx.compose.ui:ui:1.9.3")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.3")
+    implementation("androidx.compose.foundation:foundation:1.9.3")
+    implementation("androidx.compose.material3:material3:1.4.0")
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation(libs.shimmer)
     implementation(libs.palette.ktx)
     implementation(libs.tvprovider)
