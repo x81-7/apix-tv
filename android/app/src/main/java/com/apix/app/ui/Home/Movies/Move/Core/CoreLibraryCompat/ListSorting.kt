272f117b59240d087bda5b86c2f2baa840aa0b2e
x81-7 <hadeelhasson2007@gmail.com>
2026-08-22 13:40:18 +0300

    Create Core/CoreLibraryCompat/ListSorting.kt

---
+++ android/app/src/main/java/com/apix/app/ui/Home/Movies/Move/Core/CoreLibraryCompat/ListSorting.kt
@@ -0,0 +1,5 @@
+package com.apix.app.ui.Home.Movies.Move.CoreLibraryCompat
+
+enum class ListSorting {
+    Query, RatingHigh, RatingLow, AlphabeticalA, AlphabeticalZ, UpdatedNew, UpdatedOld, ReleaseDateNew, ReleaseDateOld
+}
